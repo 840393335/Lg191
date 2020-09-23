9f913610ea211502ad0815ab9b4eae77f8748580
840393335@qq.com <840393335@qq.com>
2020-09-23 11:08:23 +0800

    11112222

---
+++ mymodel/src/main/java/com/cssl/pojo/Emp.java
@@ -0,0 +1,14 @@
+package com.cssl.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Emp {
+    private Integer id;
+    private String username;
+    private String password;
+}
